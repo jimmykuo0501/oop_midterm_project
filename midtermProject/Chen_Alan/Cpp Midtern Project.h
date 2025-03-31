9e2cbfca5d8d231555484db1537c2533aae94f7a
Alan <67643439+alan101071@users.noreply.github.com>
2025-03-31 18:44:42 +0800

    Add files via upload

---
+++ midtermProject/Chen_Alan/Cpp Midtern Project.h
@@ -0,0 +1,81 @@
+/*****************************************************************//**
+ * \file   Cpp Midtern Project.h
+ * \brief  Cpp Midtern Project main part code(Header file).
+ *
+ * \author Alan
+ * \date   2025/3/13
+ *********************************************************************/
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+#include<ctime>
+#include"Global.h"
+#include"main.h"
+
+using namespace std;
+
+/**
+ * Place the chess on the given possition if it is a valid possition.
+ *
+ * \param posX Selected possition's x cordinate.
+ * \param posY Selected possition's y cordinate.
+ * \param player Current player(Black:false	White:ture).
+ * \return True if success, else false.
+ */
+bool playChess(short x, short y, bool player) {
+	if (board[y][x] == 'a') {
+		board[y][x] = (player) ? 'w' : 'b';
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/**
+ * User input state(25sec of play time limit).
+ *
+ * \return True if success, else false.
+ */
+bool userInput() {
+	time_t gameTime = time(NULL);	//Record start time.
+
+	while (time(NULL) - gameTime < 26) {
+		if (_kbhit()) {
+			switch (_getch()) {
+			case 119:
+			case 87:
+			case 72:	//Up arrow
+				if (posY > 0) posY--;
+				break;
+			case 115:
+			case 83:
+			case 80:	//Down arrow
+				if (posY < 7) posY++;
+				break;
+			case 65:
+			case 97:
+			case 75:	//Left arrow
+				if (posX > 0) posX--;
+				break;
+			case 68:
+			case 100:
+			case 77:	//Right arrow
+				if (posX < 7) posX++;
+				break;
+			case 13:	//Enter
+				if (playChess(posX, posY, player))
+					return true;
+				else
+					cout << "Possition invalid!\n";
+			}
+
+			system("CLS");
+			cout << posX << ' ' << posY << ' ' << ' ' << player << '\n';	//Debug message.
+		}
+	}
+
+	return false;
+}
